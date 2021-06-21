a82db82ade6466f986e6e8844073c14db572b808
Thiago Marchini <thiago@msn.com>
2021-06-21 14:56:19 -0300

    exercício 1 feito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redCar: false,
+      blueCar: false,
+      yellowCar: false,
+    }
+
+    this.moveCar = this.handleMoveCar.bind(this);
+  }
+
+  handleMoveCar(carColor) {
+    this.setState((prevState) => ({
+      [carColor]: !prevState[carColor],
+    }));
+  }
+
+  render() {
+    const context = {
+      redCar: this.state.redCar,
+      blueCar: this.state.blueCar,
+      yellowCar: this.state.yellowCar,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={context}>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
